ce4b80843260725bb4e622e40341ed5743b6779c
nongrate777 <nongrate777@gmail.com>
2023-08-16 12:20:59 +0200

    update

--- wp-config.php
+++ wp-config.php
@@ -14,20 +14,20 @@ define( 'LOGGED_IN_SALT',   'SYc]v_}A)*vXXNQ64Bs@Xb>_=^;5.`J]+Oi}i]}A-zN{RRy|Q|c
 define( 'NONCE_SALT',       'P#{2D!jTkSBtBO$Ued(juMr bejF n+o7.dDD <NZ!wW[lZ`lanzSO$ au|px`mi' );
 
 /* - - - - - deploy settings begin - - - - - */
-$configs_file = ( file_exists( '/var/www/html/wp-globals.json' ) ) ? '/var/www/html/wp-globals.json' : __DIR__ .'/wp-globals-dev.json';
-$configs = json_decode( file_get_contents( $configs_file ) );
+$table_prefix = 'wp_';
 
-$table_prefix = $configs->table_prefix;
+define( 'WP_DEBUG', true );
 
-define( 'WP_DEBUG', $configs->wp_debug );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_NAME', 'testovich' );
+define( 'DB_USER', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpress' );
 
-define( 'DB_HOST', $configs->db_host );
-define( 'DB_NAME', $configs->db_name );
-define( 'DB_USER', $configs->db_user );
-define( 'DB_PASSWORD', $configs->db_password );
-define( 'WP_HOME', $configs->new_domain );
-define( 'WP_SITEURL', $configs->new_domain );
-define( 'API_PATH',    $configs->api_host );
+define( 'WP_HOME', 'https://testovich.test/' );
+define( 'WP_SITEURL', 'https://testovich.test/' );
+
+define( 'API_PATH', 'https://api.avgr.it/api/' );
+
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
 define( 'ASSETS_VERSION', time() );
 
@@ -37,15 +37,6 @@ define( 'WPMS_SMTP_PASS', '63vBfMz02SlO37Oz' );
 define( 'ACF_PRO_LICENSE', 'YzM1NjI5NDE3ODE5YTlkNDQ2NTczOGJlZDM1MTdmZWExZjRmMDUwNWQ3ODk5OTNiODA5MmQy');
 
 define( 'WPMDB_LICENCE', 'c41e43c8-656e-42f4-8c92-098a417ab7e4' );
-/* - - - - - deploy settings end - - - - - */
-
-define( 'FORCE_SSL_ADMIN', true );
-define( 'FORCE_SSL_LOGIN', false );
-define( 'ALLOW_UNFILTERED_UPLOADS', true );
-
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
-}
 
 require_once( ABSPATH . 'wp-settings.php' ); //Disable File Edits
 //define( 'DISALLOW_FILE_EDIT', true );
